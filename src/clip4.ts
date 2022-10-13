f9b18ed3ba3a4a4e1a95195dcf81d3998e556c1f
no822 <nolan.kim@cobosys.co.kr>
2022-10-13 15:53:56 +0900

    css 로더 추가

--- src/clip4.ts
+++ src/clip4.ts
@@ -1,1 +1,7 @@
-console.log('clip4.ts import!');
+import { cart } from './cart';
+import './index.css';
+
+// todo if문과 for문을 이용해서 장바구니 데이터 렌더링
+
+
+console.log(cart)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 
 module.exports = {
+    mode: 'development',
     entry: "./src/index.ts",
     module: {
         rules: [
@@ -9,13 +13,33 @@ module.exports = {
                 use: "ts-loader",
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
         ],
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js"],
     },
     output: {
-        filename: "main.js",
+        filename: "index.js",
         path: path.resolve(__dirname, "dist"),
     },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ["dist", "public"],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",     // 적용될 html 경로
+            filename: "./index.html", // 결과 파일명
+            meta: {
+                // meta 태그를 추가
+                viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+            },
+            hash: true,       // 모든 스크립트, css 파일에 고유한 컴파일 해시 추가하여 캐시를 무효화
+            showErrors: true, // 오류 정보가 html에 기록됨
+        }),
+    ],
+
 };
